2acb34ab359a71968da1b96f59dc97683d0b68dc
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2023-11-21 20:19:53 +0200

    Try to format publication date

--- app/src/main/java/com/example/booksassignment/data/mappings/bookdetails/DatabaseBookDetailsMapper.kt
+++ app/src/main/java/com/example/booksassignment/data/mappings/bookdetails/DatabaseBookDetailsMapper.kt
@@ -3,6 +3,10 @@ package com.example.booksassignment.data.mappings.bookdetails
 import com.example.booksassignment.data.mappings.DtoMapper
 import com.example.booksassignment.data.models.BookDetails
 import com.example.booksassignment.data.sources.local.bookdetails.BookDetailsEntity
+import java.time.OffsetDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import javax.inject.Inject
 
 class DatabaseBookDetailsMapper @Inject constructor() : DtoMapper<BookDetailsEntity, BookDetails> {
@@ -12,11 +16,20 @@ class DatabaseBookDetailsMapper @Inject constructor() : DtoMapper<BookDetailsEnt
             id = input.id,
             listId = input.listId,
             isbn = input.isbn,
-            publicationDate = input.publicationDate,
+            publicationDate = getFormattedPublicationDate(input.publicationDate),
             author = input.author,
             title = input.title,
             img = input.img,
             description = input.description
         )
+    }
+
+    private fun getFormattedPublicationDate(publicationDate: String): String {
+        try {
+            val offsetDateTime = OffsetDateTime.parse(publicationDate)
+            return offsetDateTime.format(DateTimeFormatter.ISO_DATE)
+        } catch (_: DateTimeParseException) {
+            return publicationDate
+        }
     }
 }
